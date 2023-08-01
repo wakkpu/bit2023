fd033c04885f8c99e92d15d8eab2b00c2b88cce0
wakkpu <ho0524870@gmail.com>
2023-08-01 15:15:34 +0900

    20230801-3

--- day01/src/day01/Main.java
+++ day01/src/day01/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World 111!!");
 		System.out.println("Hello World 222!!");
 	}
 
